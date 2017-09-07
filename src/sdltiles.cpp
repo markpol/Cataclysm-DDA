e481d57de50186bbc17266477371d96df40841cf
ZhilkinSerg <zhilkin.serg@gmail.com>
2017-09-07 08:42:10 +0300

    Removed SDL_Texture_Ptr for alternate_rect_tex

--- src/sdltiles.cpp
+++ src/sdltiles.cpp
@@ -274,7 +274,7 @@ void generate_alt_rect_texture()
 #endif
     SDL_Surface_Ptr altsurf( SDL_CreateRGBSurface( 0, 1, 1, 32, rmask, gmask, bmask, amask ) );
     SDL_FillRect( altsurf.get(), NULL, SDL_MapRGB( altsurf->format, 255, 255, 255 ) );
-    SDL_Texture_Ptr alternate_rect_tex( SDL_CreateTextureFromSurface( renderer.get(), altsurf.get() ) );
+    alternate_rect_tex.reset( SDL_CreateTextureFromSurface( renderer.get(), altsurf.get() ) )
     altsurf.reset();
 
     // test to make sure color modulation is supported by renderer
