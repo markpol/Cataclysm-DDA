cddc6f9ed26e64d2f10ca3168c056c9557764909
ZhilkinSerg <ZhilkinSerg@users.noreply.github.com>
2018-01-08 14:31:10 +0300

    Astyle `limit_coordinates` function

--- src/overmapbuffer.cpp
+++ src/overmapbuffer.cpp
@@ -103,7 +103,7 @@ void overmapbuffer::create_custom_overmap( int x, int y,
     new_om->populate( specials );
 }
 
-void overmapbuffer::limit_coordinates(int & x, int & y)
+void overmapbuffer::limit_coordinates( int &x, int &y )
 {
     if( get_option<int>( "WORLD_LIMIT_X" ) > 0 ) {
         x = x % get_option<int>( "WORLD_LIMIT_X" );
