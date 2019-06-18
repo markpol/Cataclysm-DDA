07c3236e83dcab40f8df85c4a407d57202763e44
ZhilkinSerg <ZhilkinSerg@users.noreply.github.com>
2019-06-18 22:01:51 +0300

    rename

--- src/map_extras.cpp
+++ src/map_extras.cpp
@@ -2325,7 +2325,7 @@ map_extra_pointer get_function( const std::string &name )
 {
     const auto iter = builtin_functions.find( name );
     if( iter == builtin_functions.end() ) {
-        debugmsg( "no map special with name %s", name );
+        debugmsg( "no map extra function with name %s", name );
         return nullptr;
     }
     return iter->second;
